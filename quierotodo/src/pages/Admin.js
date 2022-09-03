597d09739d1e4b2a41ab3c7f21b0795529ebb68e
Matias Neira <matiasneirab@gmail.com>
2022-09-03 10:12:25 -0300

    add routes and pages

---
+++ quierotodo/src/pages/Admin.js
@@ -0,0 +1,7 @@
+const Admin = () => {
+    return (
+    <div>hola</div>
+    )
+}
+
+export default Admin;
---
+++ quierotodo/src/pages/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <div>chau</div>
+}
+
+export default Home;
