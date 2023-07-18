cdee1f1cf1b81f738799401189fa243fdb9a83f0
Matías Neira <matiasneirab@gmail.com>
2023-07-18 12:53:10 -0300

    render cards

--- frontend/quierotodo/src/components/Cards/Cards.js
+++ frontend/quierotodo/src/components/Cards/Cards.js
@@ -8,8 +8,9 @@ const CardProduct = () => {
 
     const getProducts = async () => {
         try {
-            const response = await axiosClient.get("/");
-            setProducts(response.data.coins);
+            const response = await axiosClient.get("/product");
+            setProducts(response.data);
+            console.log(products)
         } catch (error) {
             console.log(error);
         }
@@ -19,19 +20,18 @@ const CardProduct = () => {
     }, []);
     return (
         <>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://f.fcdn.app/imgs/2c03cd/www.palaciodelamusica.com.uy/pmusuy/0955/original/catalogo/012AGG4000-012AGG4000_1/2000-2000/auricular-gamer-g4000-auricular-gamer-g4000.jpg" />
-                <Card.Body>
-                    <Card.Title>Nombre del producto</Card.Title>
-                    <Card.Text>
-                        Codigo:                    </Card.Text>
-                    <Card.Text>
-                        Precio:
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            {products.map((product) => (
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={product.img} />
+                    <Card.Body>
+                        <Card.Title>{product.name}</Card.Title>
+                        <Card.Text>Codigo:{product.sku} </Card.Text>
+                        <Card.Text>Precio: {product.price}</Card.Text>
+                    </Card.Body>
+                </Card>
+            ))
+            }
         </>
-
     );
 }
 
