4e6d4903e4c50f8d62561a554c1497928f05eb09
Matías Neira <matiasneirab@gmail.com>
2023-07-18 13:56:43 -0300

    add flex cards

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,38 @@
+import { Card, Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import axiosClient from '../../config/axiosClient';
+
+const CardProduct = () => {
+
+    const [products, setProducts] = useState([]);
+
+    const getProducts = async () => {
+        try {
+            const response = await axiosClient.get("/product");
+            setProducts(response.data);
+            console.log(products)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getProducts();
+    }, []);
+    return (
+        <Container className='d-flex flex-wrap'>
+            {products.map((product) => (
+                <Card className='m-2' style={{ width: '18rem'}}>
+                    <Card.Img variant="top" src={product.img} />
+                    <Card.Body>
+                        <Card.Title>{product.name}</Card.Title>
+                        <Card.Text>Codigo:{product.sku} </Card.Text>
+                        <Card.Text>Precio: {product.price}</Card.Text>
+                    </Card.Body>
+                </Card>
+            ))
+            }
+        </Container>
+    );
+}
+
+export default CardProduct;
